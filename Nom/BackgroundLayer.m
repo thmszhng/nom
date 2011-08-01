ca70bb6015eabd67783cf7b8ffae40a0264ea8a5
Geoffry Song <goffrie@gmail.com>
2011-07-31 20:13:41 -0400

    Remove commented code dealing with removed sprites

--- Nom/BackgroundLayer.m
+++ Nom/BackgroundLayer.m
@@ -14,22 +14,10 @@
     self = [super init];
     if (self != nil)
     {
-//        CCSprite *backgroundImage;
-//        CCSprite *grid;
         CCSprite *control;
-
-//        backgroundImage = [CCSprite spriteWithFile:@"backgroundiPhone.png"];
-//        grid = [CCSprite spriteWithFile:@"Grid.png"];
         control = [CCSprite spriteWithFile:@"Control.png"];
-        
         CGSize screenSize = [[CCDirector sharedDirector] winSize];
-        
-//        [backgroundImage setPosition: CGPointMake(screenSize.width/2, screenSize.height/2)];
-//        [grid setPosition: CGPointMake(screenSize.width/2, 320)];
-        [control setPosition: CGPointMake (screenSize.width/2, 85)]; 
-        
-//        [self addChild:backgroundImage z:0 tag:kGameSceneTagValue];
-//        [self addChild:grid z:0 tag:kGameSceneTagValue];
+        [control setPosition: CGPointMake (screenSize.width/2, 85)];
         [self addChild:control z:0 tag:kGameSceneTagValue];
     }
     
