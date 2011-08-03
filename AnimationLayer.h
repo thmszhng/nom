75e9d608cd1dd35641529d60ae6c8ace4b9a2bb5
Geoffry Song <goffrie@gmail.com>
2011-08-03 00:18:07 -0400

    implement runner animation thing (AnimationLayer)
    It's under MainMenuScene for now. It isn't used anywhere (yet)

---
+++ AnimationLayer.h
@@ -0,0 +1,35 @@
+//
+//  AnimationLayer.h
+//  Nom
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+#define MAX_RUNNERS 400
+
+#define WIDTH 32
+#define HEIGHT 48
+
+@interface AnimationLayer : CCLayer 
+{
+    int X[MAX_RUNNERS];
+    int Y[MAX_RUNNERS];
+    int dX[MAX_RUNNERS];
+    int dY[MAX_RUNNERS];
+    int freeList[MAX_RUNNERS];
+    int numFree;
+    
+    int grid[WIDTH][HEIGHT];
+}
+
+-(id) init;
+
+-(int) newRunner;
+-(void) deleteRunner: (int) which;
+-(void) moveRunner: (int) which;
+
+-(void) update: (ccTime) deltaTime;
+-(void) draw;
+
+@end
