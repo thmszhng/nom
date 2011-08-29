5bc67790684318730cc46b42b1d4b49dece6dc6b
Geoffry Song <goffrie@gmail.com>
2011-08-29 15:23:19 -0400

    burst mode

---
+++ BurstMode.m
@@ -0,0 +1,36 @@
+//
+//  BurstMode.m
+//  Nom
+//
+//  Created on 11-08-29.
+//  Copyright Thomas Zhang, Geoffry Song, Eddy Gao. All rights reserved.
+//
+
+#import "BurstMode.h"
+
+#import "BurstFood.h"
+#import "FoodRandomizer.h"
+
+@implementation BurstMode
+
+-(id) init
+{
+    self = [super init];
+    if (self != nil)
+    {
+        [self createFood: [BurstFood class]];
+    }
+    return self;
+}
+
+-(void) onEat: (Food *) eaten
+{
+    // trigger a new spawn if there is no burst food left
+    for (int i = foodAmount; i--; )
+    {
+        if (food[i] != eaten && [food[i] isKindOfClass: [BurstFood class]]) return;
+    }
+    [self createFood: [BurstFood class]];
+}
+
+@end
