49c8d47e8604649530f88287bd9d71135946eb72
Thomas Zhang <twizam@gmail.com>
2012-01-04 11:33:27 -0500

    tweaked rage food

--- Sources/Game/GridObject/Food/RageFood.m
+++ Sources/Game/GridObject/Food/RageFood.m
@@ -20,11 +20,11 @@
 
 -(BOOL) eat: (Game *) game
 {
-    [game setRageExpiry:([game timestamp]+10.f)];
+    [game setRageExpiry:([game timestamp]+15/2.f)];
     if (game.isRaged) 
         return YES;
     [game setIsRaged: true];
-    [game setSpeed: [game speed]/1.75];
+    [game setSpeed: [game speed]/1.666];
     return YES;
 }
 
